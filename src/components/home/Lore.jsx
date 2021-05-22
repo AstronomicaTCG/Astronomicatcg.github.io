5396471940675accbb3934d7f70b03b8a399d483
TLeonBrown <tianbrown@gmail.com>
2021-05-21 23:44:21 -0400

    Forgot lore too lul

--- src/components/home/Lore.jsx
+++ src/components/home/Lore.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Lore = ({ heading }) => {
   return (
     <div>
-        <div className="sectionHeading" id="Lore">
+        <div className="sectionHeading" id="lore">
           <h2>{heading}</h2>
         </div>
     </div>
