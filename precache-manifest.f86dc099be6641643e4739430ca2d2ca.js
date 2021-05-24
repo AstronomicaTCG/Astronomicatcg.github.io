3c137e173b789466ac6dad54e76ed0d568e674e0
TLeonBrown <tianbrown@gmail.com>
2021-05-23 21:46:07 -0400

    Updates

---
+++ precache-manifest.f86dc099be6641643e4739430ca2d2ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eaa4fbe8abad0c11715ec8f0b107db31",
+    "url": "./index.html"
+  },
+  {
+    "revision": "993f45e03966084dd804",
+    "url": "./static/css/main.0a97a869.chunk.css"
+  },
+  {
+    "revision": "93230ad7d12c15030ef3",
+    "url": "./static/js/2.ba67c06c.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "./static/js/2.ba67c06c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "993f45e03966084dd804",
+    "url": "./static/js/main.36e4622c.chunk.js"
+  },
+  {
+    "revision": "53c8213d7beab5c9be14",
+    "url": "./static/js/runtime-main.edec85e9.js"
+  }
+]);
