77a0fcbe25f8a2946d07a89c80e30c3ae550a42a
TLeonBrown <tianbrown@gmail.com>
2023-03-03 22:38:16 -0500

    Implement pagination

--- src/components/home/Database.jsx
+++ src/components/home/Database.jsx
@@ -1,77 +1,63 @@
 import React, { useState } from "react";
-import { Jumbotron, Container, Row, Col, InputGroup, FormControl, Button, Modal } from "react-bootstrap";
+import { Jumbotron, Container, Row, Col, Button, Modal } from "react-bootstrap";
 import Card from "./Card";
+import ReactPaginate from 'react-paginate';
 
 const Database = ({ heading, cards, cardNames, cardInfo }) => {
+
+  const ITEMS_PER_PAGE = 32;
+  const [cardsList, setCardsList] = useState([]);
+
+  // Create objects for each card
+  if (cardsList.length === 0) {
+    for (let i = 0; i < cards.length; i++) {
+      cardsList.push({
+        image: cards[i],
+        name: cardNames[i],
+        rarity: cardInfo[i].rarity,
+        set: cardInfo[i].set,
+        type: cardInfo[i].type,
+        faction: cardInfo[i].faction,
+        visible: true,
+      })
+    }
+  }
 
   const [displayedCardInfo, setDisplayedCardInfo] = useState(["", "", "", ""]);
   const [showCardModal, setShowCardModal] = useState(false);
   const [showCardURL, setShowCardURL] = useState("");
 
-  const [filterByType, setFilterByType] = useState({ units: true, munitions: true, events: true, planets: true }); // Units, Munitions, Events, Planets
-  const [filterByRarity, setFilterByRarity] = useState({ ar: true, sr: true, cr: true, r: true, uc: true, c: true, fp: true, p: true }); // AR, SR, CR, R, UC, C, FP, P
-  const [filterBySet, setFilterBySet] = useState({ ee: true, hf: true, sd: true }); // EE, HF, SD
-  const [filterByName, setFilterByName] = useState("");
-  const [filterByFaction, setFilterByFaction] = useState({ lc: true, kc: true, bf: true, vy: true }) // LC, KC, BF, VY
+  // Taken from React Paginate API
+  const [itemOffset, setItemOffset] = useState(0);
 
-  // cards.reverse()
-  // cardNames.reverse()
-  // cardInfo.reverse()
+  const endOffset = itemOffset + ITEMS_PER_PAGE;
+  let currentCards = cardsList.slice(itemOffset, endOffset);
+  let pageCount = Math.ceil(cardsList.length / ITEMS_PER_PAGE);
+
+  // Invoke when user click to request another page.
+  const handlePageClick = (event) => {
+    const newOffset = (event.selected * ITEMS_PER_PAGE) % cardsList.length;
+    setItemOffset(newOffset);
+  };
 
   // Turn all checkboxes on or off, and update states accordingly.
-  const toggleAllSelections = (val) => {
+  const toggleAllSelections = val => {
     let checkboxes = document.getElementsByClassName("checkBox");
-    for (let i = 0; i < checkboxes.length; i++) {
+    for (let i = 0, n = checkboxes.length; i < n; i++) {
       checkboxes[i].checked = val;
     }
-    setFilterByType({ units: val, munitions: val, events: val, planets: val });
-    setFilterByRarity({ ar: val, sr: val, cr: val, r: val, uc: val, c: val, fp: val, p: val });
-    setFilterBySet({ ee: val, hf: val, sd: val });
-    setFilterByFaction({ lc: val, kc: val, bf: val, vy: val });
-
+    for (let i = 0, n = cardsList.length; i < n; i++) {
+      cardsList[i].visible = val
+    }
   }
 
   // Turn a specific checkbox on or off, and update state accordingly.
-  const toggleFilter = (filterType, subtype) => {
-    if (filterType === "type") {
-      let items = { ...filterByType };
-      items[subtype] = !items[subtype];
-      setFilterByType(items);
+  const toggleFilter = (filterType, subType) => {
+    for (let i = 0, n = cardsList.length; i < n; i++) {
+      if (cardsList[i][filterType] === subType) {
+        cardsList[i].visible = !cardsList[i].visible
+      }
     }
-    else if (filterType === "rarity") {
-      let items = { ...filterByRarity };
-      items[subtype] = !items[subtype];
-      setFilterByRarity(items);
-    }
-    else if (filterType === "faction") {
-      let items = { ...filterByFaction };
-      items[subtype] = !items[subtype];
-      setFilterByFaction(items);
-    }
-    else {
-      let items = { ...filterBySet };
-      items[subtype] = !items[subtype];
-      setFilterBySet(items);
-    }
-  }
-
-  // Clear the name filter and its contents.
-  const clearTextFilter = () => {
-    setFilterByName("");
-    document.getElementById("cardTextFilter").value = "";
-  }
-
-  // Callback function used for filtering cards.
-  function cardFilter(element, index) {
-    let checkBoxFilters =
-      filterByType[cardInfo[index].type.toLowerCase()] &&
-      filterByRarity[cardInfo[index].rarity.toLowerCase()] &&
-      filterBySet[cardInfo[index].set.toLowerCase()] &&
-      filterByFaction[cardInfo[index].faction.toLowerCase()];
-    if (filterByName !== "") {
-      return checkBoxFilters && cardNames[index].toLowerCase().includes(filterByName.toLowerCase());
-    }
-    return checkBoxFilters;
   }
 
   return (
@@ -83,40 +69,29 @@ const Database = ({ heading, cards, cardNames, cardInfo }) => {
           </h2>
 
           <div className="database">
-
             <Container className="databaseInfoPanel">
               <div>
-                <InputGroup className="mb-3">
-                  <FormControl
-                    id="cardTextFilter"
-                    placeholder="Search for a card..."
-                    aria-label="Search for a card..."
-                    aria-describedby="basic-addon2"
-                    onKeyUp={(e) => setFilterByName(e.target.value)}
-                  ></FormControl>
-                  &nbsp; <Button variant="dark" onClick={clearTextFilter}>Clear</Button>
-                </InputGroup>
                 <div className="howToPlayTextHeading">Filter By</div>
                 <br></br>
                 <div className="databaseFilters">
                   <div className="checkBoxColumn">
                     <b className="databaseCheckboxHeader">Type</b>
-                    <label className="checkBoxLabel"><input defaultChecked className="checkBox" type="checkbox" onChange={() => toggleFilter("type", "units")} />Units</label>
-                    <label className="checkBoxLabel"><input defaultChecked className="checkBox" type="checkbox" onChange={() => toggleFilter("type", "munitions")} />Munitions</label>
-                    <label className="checkBoxLabel"><input defaultChecked className="checkBox" type="checkbox" onChange={() => toggleFilter("type", "events")} />Events</label>
-                    <label className="checkBoxLabel"><input defaultChecked className="checkBox" type="checkbox" onChange={() => toggleFilter("type", "planets")} />Planets</label>
+                    <label className="checkBoxLabel"><input defaultChecked className="checkBox" type="checkbox" onChange={() => toggleFilter("type", "Units")} />Units</label>
+                    <label className="checkBoxLabel"><input defaultChecked className="checkBox" type="checkbox" onChange={() => toggleFilter("type", "Munitions")} />Munitions</label>
+                    <label className="checkBoxLabel"><input defaultChecked className="checkBox" type="checkbox" onChange={() => toggleFilter("type", "Events")} />Events</label>
+                    <label className="checkBoxLabel"><input defaultChecked className="checkBox" type="checkbox" onChange={() => toggleFilter("type", "Planets")} />Planets</label>
                   </div>
                   <br></br>
                   <div className="checkBoxColumn">
                     <b className="databaseCheckboxHeader">Rarity</b>
-                    <label className="checkBoxLabel"><input defaultChecked className="checkBox" type="checkbox" onChange={() => toggleFilter("rarity", "ar")} />Astral Rare</label>
-                    <label className="checkBoxLabel"><input defaultChecked className="checkBox" type="checkbox" onChange={() => toggleFilter("rarity", "sr")} />Super Rare</label>
-                    <label className="checkBoxLabel"><input defaultChecked className="checkBox" type="checkbox" onChange={() => toggleFilter("rarity", "cr")} />Crossover Rare</label>
-                    <label className="checkBoxLabel"><input defaultChecked className="checkBox" type="checkbox" onChange={() => toggleFilter("rarity", "r")} />Rare</label>
-                    <label className="checkBoxLabel"><input defaultChecked className="checkBox" type="checkbox" onChange={() => toggleFilter("rarity", "uc")} />Uncommon</label>
-                    <label className="checkBoxLabel"><input defaultChecked className="checkBox" type="checkbox" onChange={() => toggleFilter("rarity", "c")} />Common</label>
-                    <label className="checkBoxLabel"><input defaultChecked className="checkBox" type="checkbox" onChange={() => toggleFilter("rarity", "fp")} />Full-Art Planet</label>
-                    <label className="checkBoxLabel"><input defaultChecked className="checkBox" type="checkbox" onChange={() => toggleFilter("rarity", "p")} />Planet</label>
+                    <label className="checkBoxLabel"><input defaultChecked className="checkBox" type="checkbox" onChange={() => toggleFilter("rarity", "AR")} />Astral Rare</label>
+                    <label className="checkBoxLabel"><input defaultChecked className="checkBox" type="checkbox" onChange={() => toggleFilter("rarity", "SR")} />Super Rare</label>
+                    <label className="checkBoxLabel"><input defaultChecked className="checkBox" type="checkbox" onChange={() => toggleFilter("rarity", "CR")} />Crossover Rare</label>
+                    <label className="checkBoxLabel"><input defaultChecked className="checkBox" type="checkbox" onChange={() => toggleFilter("rarity", "R")} />Rare</label>
+                    <label className="checkBoxLabel"><input defaultChecked className="checkBox" type="checkbox" onChange={() => toggleFilter("rarity", "UC")} />Uncommon</label>
+                    <label className="checkBoxLabel"><input defaultChecked className="checkBox" type="checkbox" onChange={() => toggleFilter("rarity", "C")} />Common</label>
+                    <label className="checkBoxLabel"><input defaultChecked className="checkBox" type="checkbox" onChange={() => toggleFilter("rarity", "FP")} />Full-Art Planet</label>
+                    <label className="checkBoxLabel"><input defaultChecked className="checkBox" type="checkbox" onChange={() => toggleFilter("rarity", "P")} />Planet</label>
                   </div>
                   <br></br>
                   <div className="checkBoxColumn">
@@ -128,15 +103,14 @@ const Database = ({ heading, cards, cardNames, cardInfo }) => {
                   <br></br>
                   <div className="checkBoxColumn">
                     <b className="databaseCheckboxHeader">Faction</b>
-                    <label className="checkBoxLabel"><input defaultChecked className="checkBox" type="checkbox" onChange={() => toggleFilter("faction", "lc")} />Legion Corps</label>
-                    <label className="checkBoxLabel"><input defaultChecked className="checkBox" type="checkbox" onChange={() => toggleFilter("faction", "kc")} />Kakytos Council</label>
-                    <label className="checkBoxLabel"><input defaultChecked className="checkBox" type="checkbox" onChange={() => toggleFilter("faction", "bf")} />Babylon Force</label>
-                    <label className="checkBoxLabel"><input defaultChecked className="checkBox" type="checkbox" onChange={() => toggleFilter("faction", "vy")} />Factionless</label>
+                    <label className="checkBoxLabel"><input defaultChecked className="checkBox" type="checkbox" onChange={() => toggleFilter("faction", "LC")} />Legion Corps</label>
+                    <label className="checkBoxLabel"><input defaultChecked className="checkBox" type="checkbox" onChange={() => toggleFilter("faction", "KC")} />Kakytos Council</label>
+                    <label className="checkBoxLabel"><input defaultChecked className="checkBox" type="checkbox" onChange={() => toggleFilter("faction", "BF")} />Babylon Force</label>
+                    <label className="checkBoxLabel"><input defaultChecked className="checkBox" type="checkbox" onChange={() => toggleFilter("faction", "VY")} />Factionless</label>
                   </div>
                 </div>
 
                 <div>
-
                 </div>
                 <Button variant="dark" onClick={() => toggleAllSelections(true)}>Select All</Button> &nbsp;
                 <Button variant="dark" onClick={() => toggleAllSelections(false)}>Clear All</Button>
@@ -149,26 +123,41 @@ const Database = ({ heading, cards, cardNames, cardInfo }) => {
               </div>
             </Container>
 
-            <Container className="databaseScrollContainer">
-              <Row xs={2} md={4}>
-                {cards.filter(cardFilter).map((card, index) => (
-                  <Col className="databaseCol">
-                    <Card
-                      index={index}
-                      imageUrl={card}
-                      cardName={cardNames.filter(cardFilter)[index]}
-                      rarity={cardInfo.filter(cardFilter)[index].rarity}
-                      faction={cardInfo.filter(cardFilter)[index].faction}
-                      set={cardInfo.filter(cardFilter)[index].set}
-                      type={cardInfo.filter(cardFilter)[index].type}
-                      setDisplayedCardInfo={setDisplayedCardInfo}
-                      setShowCardModal={setShowCardModal}
-                      setShowCardURL={setShowCardURL}
-                    ></Card>
-                  </Col>
-                ))}
-              </Row>
-            </Container>
+            <div className='databaseRightSideContainer'>
+              <Container className="databaseScrollContainer">
+                <Row xs={2} md={4}>
+                  {currentCards.map((card, index) => (
+                    card.visible ?
+                      <Col className="databaseCol">
+                        <Card
+                          index={index}
+                          imageUrl={card.image}
+                          cardName={card.name}
+                          rarity={card.rarity}
+                          faction={card.faction}
+                          set={card.set}
+                          type={card.type}
+                          setDisplayedCardInfo={setDisplayedCardInfo}
+                          setShowCardModal={setShowCardModal}
+                          setShowCardURL={setShowCardURL}
+                        >
+                        </Card>
+                      </Col> : ''
+                  ))}
+                </Row>
+              </Container>
+              <p className="centeredText">Page {(itemOffset / ITEMS_PER_PAGE) + 1}</p>
+              <ReactPaginate
+                className='databasePagination'
+                breakLabel="..."
+                nextLabel="Next"
+                onPageChange={handlePageClick}
+                pageRangeDisplayed={3}
+                pageCount={pageCount}
+                previousLabel="Prev"
+                renderOnZeroPageCount={null}
+              />
+            </div>
 
             <Modal className="cardModal"
               show={showCardModal}
